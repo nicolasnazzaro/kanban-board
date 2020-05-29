b75b36f1d01f454125451ff08435cafc1ae6c32d
nicolasnazzaro <nicolas.nazzaro@gmail.com>
2020-05-29 20:39:04 +0200

    Updates

---
+++ precache-manifest.e57cba41ff79ac354489c9cbbc4c8513.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd1a4145cccc7be8f5ba44a782be1d0",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "665be0c5280ddd5d28c1",
+    "url": "/kanban-board/static/js/2.44ffd167.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kanban-board/static/js/2.44ffd167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfce0d0221d47f3c0af",
+    "url": "/kanban-board/static/js/main.1212d3a4.chunk.js"
+  },
+  {
+    "revision": "df7f5190008ec2303916",
+    "url": "/kanban-board/static/js/runtime-main.6be55f1a.js"
+  }
+]);
